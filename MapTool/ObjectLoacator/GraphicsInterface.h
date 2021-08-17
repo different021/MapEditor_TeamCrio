41f546f932ebc6f4b03ff3af157872da24839ce4
different021 <differentg021@gmail.com>
2021-08-17 16:40:46 +0900

    Design GraphicEngine Interface. It's for Graphic Engine Independency Editor

--- MapTool/ObjectLoacator/GraphicsInterface.h
+++ MapTool/ObjectLoacator/GraphicsInterface.h
@@ -1,38 +1,39 @@
 #pragma once
 #include <Windows.h>	//HWND
 
-class object;
 class GraphicObject;
+struct object;
 
 class GraphicCollider;
 struct collider;
 
 class GraphicLight;
-class lightData;
+struct lightData;
 
 class GraphicWave;
-class waveData;
+struct waveData;
 
-//__interface란 키워드 자체가 윈도우즈 전용.
-__interface IGraphicsInterface
+//__interface란 키워드 자체가 윈도우즈 전용. 
+class IGraphicsInterface abstract
 {
 public:
 	//Create/Initialize Interface Instance
-	virtual IGraphicsInterface* CreateInstance();
-	virtual bool Initialize(HWND hTargetWnd, int iWidth, int iHeight);		//HWND 추상화 가능?
+	virtual IGraphicsInterface* CreateInstance() = 0;
+	virtual bool Initialize(HWND hTargetWnd, int iWidth, int iHeight) = 0;		//HWND 추상화 가능?
 
 	//엔진 내부에서 전체 오브젝트를 그리는 기능 요구
-	virtual void Draw();	
+	virtual void Draw() = 0;	 
 
 	//Create Graphics Instance
-	virtual GraphicObject* CreateGraphicObject(object* pObj);			//
-	virtual GraphicLight*  CreateGraphicLight(light* pLight);
-	virtual GraphicWave*   CreateGraphicWave(wave* pWave);
+	virtual GraphicObject*   CreateGraphicObject(object* pObj) = 0;			
+	virtual GraphicCollider* CreateGraphicCollider(collider* pCollider) = 0;
+	virtual GraphicLight*    CreateGraphicLight(lightData* pLight) = 0;
+	virtual GraphicWave*     CreateGraphicWave(waveData* pWave) = 0;
 
 	//Picking
-	virtual GraphicObject* PickingObject(unsigned int x, unsigned int y);
+	virtual GraphicObject* PickingObject(unsigned int x, unsigned int y) = 0;
 
 	//아직 디자인 되지 않음.
-	virtual void LoadModel();
-	virtual void LoadMaterial();
+	virtual void LoadModel() = 0;
+	virtual void LoadMaterial() = 0;
 };
